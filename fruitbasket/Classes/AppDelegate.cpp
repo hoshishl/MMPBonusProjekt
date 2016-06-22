275d0448e2d6d242f8fcd6218619a2928517de62
hoshishl <e.moellertelicsk@campus.lmu.de>
2016-06-22 22:55:33 +0200

    screen size angepasst

--- fruitbasket/Classes/AppDelegate.cpp
+++ fruitbasket/Classes/AppDelegate.cpp
@@ -6,10 +6,10 @@ USING_NS_CC;
 
 
 
-static cocos2d::Size designResolutionSize = cocos2d::Size(800, 1000);
-static cocos2d::Size smallResolutionSize = cocos2d::Size(400, 500);
-static cocos2d::Size mediumResolutionSize = cocos2d::Size(400, 500);
-static cocos2d::Size largeResolutionSize = cocos2d::Size(400, 500);
+static cocos2d::Size designResolutionSize = cocos2d::Size(1024, 600);
+static cocos2d::Size smallResolutionSize = cocos2d::Size(1024, 600);
+static cocos2d::Size mediumResolutionSize = cocos2d::Size(1024, 600);
+static cocos2d::Size largeResolutionSize = cocos2d::Size(1024, 600);
 
 AppDelegate::AppDelegate() {
 
